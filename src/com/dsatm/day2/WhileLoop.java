c763337b34da7fb2d81f6ff538e80d74caeb8313
Kiran17Shetty <kirankg571@gmail.com>
2025-09-22 14:24:11 +0530

    Day 2 completed

---
+++ src/com/dsatm/day2/WhileLoop.java
@@ -0,0 +1,13 @@
+package com.dsatm.day2;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int i=97;
+        while(i<=100) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
--- src/com/dsatm/day2/firstPogram.java
+++
@@ -1,7 +0,0 @@
-package com.dsatm.day2;
-
-public class firstPogram {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-    }
-}
