356b4c8ba679f54262c4bb493228112e59638226
Kiran17Shetty <kirankg571@gmail.com>
2025-10-23 16:40:20 +0530

    ExceptionHandling

---
+++ src/com/dsatm/ExceptionHandling/TryCatchDemo.java
@@ -0,0 +1,10 @@
+package com.dsatm.ExceptionHandling;
+
+public class TryCatchDemo {
+    public static void main(String[] args){
+        System.out.println();
+        int result;
+        TryCatchExample.performDivision(12,0);
+        System.out.println(TryCatchExample.performDivision(12.6f, 13.4f));
+    }
+}
