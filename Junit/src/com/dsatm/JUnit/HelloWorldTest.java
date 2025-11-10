485ee541ee1591e4d8e787ffee5e86c43b490f4b
Kiran17Shetty <kirankg571@gmail.com>
2025-11-10 16:50:19 +0530

    Adding Junit Folder with Junit Annotation Execution

---
+++ Junit/src/com/dsatm/JUnit/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.dsatm.JUnit;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    @Test
+    void test(){
+        System.out.println("This is the first test case");
+    }
+    @Test
+    void test1(){
+        System.out.println("This is the second test case");
+    }
+    void test2()
+    {
+        System.out.println("this is the third test case");
+    }
+}
